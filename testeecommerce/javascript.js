01afb9fb3cc0cf1daca7616ff7b2d254d0bee9f7
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2016-11-25 08:51:28 -0200

    Feito correta identação e reparos no html e css

--- testeecommerce/javascript.js
+++ testeecommerce/javascript.js
@@ -1,9 +1,13 @@
-var carousel = new ch.Carousel(ch('.demo-carousel')[0], {
-    pagination: true
-});
 
+document.addEventListener('DOMContentLoaded', function(e) {
 var zoom1 = new ch.Zoom(ch('#zoom-default')[0]);
 
-var zoom2 = new ch.Zoom(ch('#zoom-preload')[0]);
-// Preload image
-zoom2.loadImage();
+});
+
+document.addEventListener('DOMContentLoaded', function(e) {
+var carousel = new ch.Carousel(ch('.demo-carousel')[0], {pagination: true});
+});
+
+var tabs = new ch.Tabs(ch(".demo-tabs")[0]);
+
+
